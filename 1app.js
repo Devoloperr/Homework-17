04481a5828ad36228cc359cee06506c887675497
Devoloperr <mirkamilezizli085@gmail.com>
2024-06-19 14:37:58 +0400

    Homework 17

---
+++ 1app.js
@@ -0,0 +1,6 @@
+let text = "Baku code center";
+console.log(text.length)
+console.log(text.includes("e"));
+console.log(text.startsWith("code"));
+console.log(text.endsWith("code"));
+console.log(text.replaceAll("t", "####"));
---
+++ app.js
@@ -0,0 +1,3 @@
+let fullName = "Mirkamil Azizli";
+let word = fullName.split(" ")[0] + fullName.split(" ")[1];
+console.log(word.length);
